73ad5ed2a4b410c2b273095b28b358022283993c
devilop239 <116188801+devilop239@users.noreply.github.com>
2025-05-25 15:26:24 +0530

    main.py

--- VenomX/main.py
+++ VenomX/main.py
@@ -1,4 +1,3 @@
-
 #
 # All rights reserved.
 import importlib
@@ -8,10 +7,11 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from config import BANNED_USERS
-from VenomX import HELPABLE, LOGGER, app, userbot
+from VenomX import HELPABLE, app, userbot
 from VenomX.core.call import Ayush
 from VenomX.plugins import ALL_MODULES
 from VenomX.utils.database import get_banned_users, get_gbanned
+from VenomX.logger_config import LOGGER  # ✅ updated import here
 
 
 async def init():
@@ -20,10 +20,12 @@ async def init():
             "No Assistant Clients Vars Defined!.. Exiting Process."
         )
         return
+
     if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
         LOGGER("VenomX").warning(
             "No Spotify Vars defined. Your bot won't be able to play spotify queries."
         )
+
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -33,17 +35,21 @@ async def init():
             BANNED_USERS.add(user_id)
     except Exception:
         pass
+
     await app.start()
+
     for all_module in ALL_MODULES:
         imported_module = importlib.import_module(all_module)
-
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    LOGGER("VenomX.plugins").info("Successfully Imported All Modules ")
+
+    LOGGER("VenomX.plugins").info("Successfully Imported All Modules")
+
     await userbot.start()
     await Ayush.start()
-    LOGGER("VenomX").info("Assistant Started Sucessfully")
+    LOGGER("VenomX").info("Assistant Started Successfully")
+
     try:
         await Ayush.stream_call(
             "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
@@ -56,11 +62,12 @@ async def init():
 
     await Ayush.decorators()
     LOGGER("VenomX").info("VenomX Started Successfully")
+
     await idle()
+
     await app.stop()
     await userbot.stop()
 
 
 if __name__ == "__main__":
     app.run(init())
-    LOGGER("VenomX").info("Stopping VenomX! GoodBye")
